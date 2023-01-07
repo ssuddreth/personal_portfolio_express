9d4654d790ed5d655273938960aed0b4ee80fe2c
Steven Suddreth <stevensuddreth@gmail.com>
2023-01-06 20:20:43 -0500

    update files for vercel deployment

--- api/index.js
+++ api/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname, 'assets')));
+const port = process.env.PORT || 5000;
 
 // sendFile will go here
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  });
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log('Server started at port 5000');
+
+module.exports = app;
